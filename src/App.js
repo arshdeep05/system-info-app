bba9cdd402cfb25e3715f0791521b8732d772117
arshdeepsynergy <93897206+arshdeepsynergy@users.noreply.github.com>
2025-05-19 15:49:40 +0530

    url pacakage added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
-import React, { useState, useEffect } from "react";
-
+import React, { useState, useEffect, useRef } from "react";
+import { ConfirmDialog, confirmDialog } from "primereact/confirmdialog";
+import { Toast } from "primereact/toast";
+import { Button } from "primereact/button";
 function App() {
   const [sysInfo, setSysInfo] = useState(null);
+  const toast = useRef(null);
 
+  const accept = () => {
+    toast.current.show({
+      severity: "info",
+      summary: "Confirmed",
+      detail: "You have accepted",
+      life: 3000,
+    });
+  };
+
+  const reject = () => {
+    toast.current.show({
+      severity: "warn",
+      summary: "Rejected",
+      detail: "You have rejected",
+      life: 3000,
+    });
+  };
+
+  const confirm1 = () => {
+    confirmDialog({
+      message: "Are you sure you want to proceed?",
+      header: "Confirmation",
+      icon: "pi pi-exclamation-triangle",
+      defaultFocus: "accept",
+      accept,
+      reject,
+    });
+  };
+
+  const confirm2 = () => {
+    confirmDialog({
+      message: "Do you want to delete this record?",
+      header: "Delete Confirmation",
+      icon: "pi pi-info-circle",
+      defaultFocus: "reject",
+      acceptClassName: "p-button-danger",
+      accept,
+      reject,
+    });
+  };
   useEffect(() => {
     const getSystemData = async () => {
       if (window.electronAPI) {
@@ -10,15 +53,31 @@ function App() {
         setSysInfo(info);
       }
     };
-    getSystemData()
+    getSystemData();
   }, []);
 
   if (!sysInfo) return <div>Loading system info...</div>;
 
   return (
     <div style={{ fontFamily: "Arial", padding: 20 }}>
+      <Toast ref={toast} />
+      <ConfirmDialog />
+      <div className="card flex flex-wrap gap-2 justify-content-center">
+        <Button
+          onClick={confirm1}
+          icon="pi pi-check"
+          label="Confirm"
+          className="mr-2"
+        ></Button>
+        <Button
+          onClick={confirm2}
+          icon="pi pi-times"
+          label="Delete"
+        ></Button>
+      </div>
+
       <h2>System Information</h2>
-      <ul>
+      <ul className="flex gap-3 flex-column">
         <li>
           <b>Platform:</b> {sysInfo.platform}
         </li>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import "primeflex/primeflex.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
